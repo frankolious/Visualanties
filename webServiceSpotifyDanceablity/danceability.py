88860248825f6eaec6cff00f0ccb4ff85deeb14d
frankolious <frankolious@gmail.com>
2022-12-03 16:12:39 +0200

    a small spotify test -failed auth however

---
+++ webServiceSpotifyDanceablity/danceability.py
@@ -0,0 +1,6 @@
+curl --request GET \
+  --url https://api.spotify.com/v1/audio-features/id \
+  --header 'Authorization:BQCZlNmfcZNd7GunOP0--' \
+  --header 'Content-Type: application/json'
+
+#api tokens only last 1hour sort safe
